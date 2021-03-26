66a185c6ea3a66a50763d7da8c8455043c8c3268
Joris van der Heijden <405620@student.fontys.nl>
2021-03-26 11:58:10 +0100

    Renamed Initial Project

---
+++ UserServiceTests/Logic/AuthenticationLogicTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserService.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Logic.Tests
+{
+    [TestClass()]
+    public class AuthenticationLogicTests
+    {
+    }
+}
