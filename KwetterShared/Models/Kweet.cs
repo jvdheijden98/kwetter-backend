5ad75dc23d8d8384630344abd8d9d74ec95f7b38
Joris van der Heijden <405620@student.fontys.nl>
2021-05-26 23:56:19 +0200

    - Timeline Pipeline YAMLs added
    - KwetterShared location where it shouldve been

---
+++ KwetterShared/Models/Kweet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KwetterShared.Models
+{
+    public class Kweet
+    {
+        public int KweetID { get; set; }
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public int Likes { get; set; }
+        public long TimeCreated { get; set; }
+        public int UserID { get; set; }
+    }
+}
